463296c5a1bb8c17b3b0d00994ec967bf4dfc3ac
Nicola Ayan <info@nicolaayan.com>
2019-02-27 15:36:36 +1100

    Initial commit

---
+++ AlloyEpiMeetup/Models/Pages/ISearchPage.cs
@@ -0,0 +1,9 @@
+namespace AlloyEpiMeetup.Models.Pages
+{
+    /// <summary>
+    /// Marker interface for search implementation
+    /// </summary>
+    public interface ISearchPage
+    {
+    }
+}
